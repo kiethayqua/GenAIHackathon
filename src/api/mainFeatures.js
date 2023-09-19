c441bfb368338c64c30e47f732ea83eb181b27bd
hung.dao11 <hung.dao1@mservice.com.vn>
2023-09-19 13:24:54 +0700

    create new route find job for cv

--- src/api/mainFeatures.js
+++ src/api/mainFeatures.js
@@ -1,226 +1,220 @@
-const express = require("express");
-const request = require("request");
-const { google } = require("googleapis");
-const fs = require("fs");
-const { convertPdfToText } = require("../../convertPdfToText");
-const { verifyToken } = require("../middlewares/auth");
-const { getOAuth2Client } = require('../utils/oauth2Client');
-const User = require('../models/UserModel');
-const JobDescription = require('../models/JobDescriptionModel');
+const express = require('express')
+const request = require('request')
+const { google } = require('googleapis')
+const fs = require('fs')
+const { convertPdfToText } = require('../../convertPdfToText')
+const { verifyToken } = require('../middlewares/auth')
+const { getOAuth2Client } = require('../utils/oauth2Client')
+const User = require('../models/UserModel')
+const JobDescription = require('../models/JobDescriptionModel')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-const router = express.Router();
+const router = express.Router()
 
 const azure = {
   apiKey: process.env.AZURE_API_KEY,
-  version: "2023-05-15",
-  resourceName: "momo-genai-17",
-  gpt35: "gpt-35",
-  gpt4: "gpt-4",
-};
+  version: '2023-05-15',
+  resourceName: 'momo-genai-17',
+  gpt35: 'gpt-35',
+  gpt4: 'gpt-4',
+}
 
 const chatGPTAzure = (prompt) => {
-  prompt = prompt.replace("“", '"');
+  prompt = prompt.replace('“', '"')
   const payload = {
-    messages: [{ role: "user", content: prompt }],
-  };
-  const uri = `https://${azure.resourceName}.openai.azure.com/openai/deployments/${azure.gpt4}/chat/completions?api-version=${azure.version}`;
+    messages: [{ role: 'user', content: prompt }],
+  }
+  const uri = `https://${azure.resourceName}.openai.azure.com/openai/deployments/${azure.gpt4}/chat/completions?api-version=${azure.version}`
 
   return new Promise((resolve) => {
     request.post(
       uri,
       {
         headers: {
-          "Content-Type": "application/json",
-          "api-key": azure.apiKey,
+          'Content-Type': 'application/json',
+          'api-key': azure.apiKey,
         },
         json: payload,
       },
       (err, res) => {
         if (err) {
-          resolve(`Error: ${err}`);
+          resolve(`Error: ${err}`)
         }
 
         if (res.statusCode == 200) {
-          const data = res.body;
-          const text = data["choices"][0]["message"]["content"];
-          resolve(text);
+          const data = res.body
+          const text = data['choices'][0]['message']['content']
+          resolve(text)
         } else {
-          resolve("");
+          resolve('')
         }
       }
-    );
-  });
-};
+    )
+  })
+}
 
-router.post(
-  "/generate/job-description",
-  verifyToken,
-  async (req, res) => {
-    try {
-      const { jobTitle = "", skills = [], extras = "" } = req.body;
+router.post('/generate/job-description', verifyToken, async (req, res) => {
+  try {
+    const { jobTitle = '', skills = [], extras = '' } = req.body
 
-      const prompt = `
+    const prompt = `
       Please generate a job description for a ${jobTitle}. 
       The ideal candidate should have experience in ${skills.join()}. 
       ${extras}.
-      Please include the job responsibilities and required qualifications.`;
+      Please include the job responsibilities and required qualifications.`
 
-      const result = await chatGPTAzure(prompt);
-      const oauth2Client = getOAuth2Client();
-      oauth2Client.setCredentials(req.body.token);
-      const drive = google.drive({ version: 'v3', auth: oauth2Client });
-      const googleUser = (await drive.about.get({ fields: 'user' })).data.user;
-      const user = await User.findOne({ id: googleUser.permissionId });
-      await JobDescription.create({
-        jobTitle,
-        data: result,
-        createdBy: user
-      });
+    const result = await chatGPTAzure(prompt)
+    const oauth2Client = getOAuth2Client()
+    oauth2Client.setCredentials(req.body.token)
+    const drive = google.drive({ version: 'v3', auth: oauth2Client })
+    const googleUser = (await drive.about.get({ fields: 'user' })).data.user
+    const user = await User.findOne({ id: googleUser.permissionId })
+    await JobDescription.create({
+      jobTitle,
+      data: result,
+      createdBy: user,
+    })
 
-      res.json({
-        data: result
-      });
-    } catch (e) {
-      res.status(500).send('Generate JD was failed!');
-    }
-  });
+    res.json({
+      data: result,
+    })
+  } catch (e) {
+    res.status(500).send('Generate JD was failed!')
+  }
+})
 
-router.post("/check-CV-for-job", async (req, res) => {
-  const { jobTitle = "" } = req.body;
-  console.log("jobTitle", jobTitle);
-  const cvPrompt = await loadCVPrompt();
-  console.log("cvPrompt", cvPrompt);
+router.post('/check-CV-for-job', async (req, res) => {
+  const { jobTitle = '' } = req.body
+  console.log('jobTitle', jobTitle)
+  const cvPrompt = await loadCVPrompt()
+  console.log('cvPrompt', cvPrompt)
 
   const prompt = `
       I have a job with description: ${jobTitle}. 
       The candidate CV: ${cvPrompt}. 
-      Check is this CV suitable for this job? Why?`;
+      Check is this CV suitable for this job? Why?`
 
-  const result = await chatGPTAzure(prompt);
-  res.send(result);
-});
+  const result = await chatGPTAzure(prompt)
+  res.send(result)
+})
 
-router.post(
-  "/test-download-pdf",
-  verifyToken,
-  async (req, res) => {
-    try {
-      const oauth2Client = getOAuth2Client();
-      oauth2Client.setCredentials(req.body.token);
-      const drive = google.drive({
-        version: "v3",
-        auth: oauth2Client,
-      });
+router.post('/test-download-pdf', verifyToken, async (req, res) => {
+  try {
+    const oauth2Client = getOAuth2Client()
+    oauth2Client.setCredentials(req.body.token)
+    const drive = google.drive({
+      version: 'v3',
+      auth: oauth2Client,
+    })
 
-      drive.files.list((err, response) => {
-        if (err) {
-          console.error("Error listing files:", err);
-          res.send("Error listing files");
-        }
-        const files = response.data.files;
-        if (files.length) {
-          const pdfFileId = files.filter(
-            (file) => file.mimeType == "application/pdf"
-          )[0].id;
-          const dest = fs.createWriteStream("test.pdf");
-          drive.files.get(
-            { fileId: pdfFileId, alt: "media" },
-            { responseType: "stream" },
-            (err, { data }) => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              data
-                .on("end", () => console.log("Done."))
-                .on("error", (err) => {
-                  console.log(err);
-                  return process.exit();
-                })
-                .pipe(dest);
+    drive.files.list((err, response) => {
+      if (err) {
+        console.error('Error listing files:', err)
+        res.send('Error listing files')
+      }
+      const files = response.data.files
+      if (files.length) {
+        const pdfFileId = files.filter(
+          (file) => file.mimeType == 'application/pdf'
+        )[0].id
+        const dest = fs.createWriteStream('test.pdf')
+        drive.files.get(
+          { fileId: pdfFileId, alt: 'media' },
+          { responseType: 'stream' },
+          (err, { data }) => {
+            if (err) {
+              console.log(err)
+              return
             }
-          );
+            data
+              .on('end', () => console.log('Done.'))
+              .on('error', (err) => {
+                console.log(err)
+                return process.exit()
+              })
+              .pipe(dest)
+          }
+        )
 
-          res.json(
-            {
-              message: `Files found:\n${files
-                .map((file) => `${file.name} (${file.id})`)
-                .join("\n")}`
-            }
-          );
-        } else {
-          res.json({ message: "No files found." });
-        }
-      });
-    } catch (e) {
-      res.status(500).send('Fail to download pdf!');
-    }
-  });
+        res.json({
+          message: `Files found:\n${files
+            .map((file) => `${file.name} (${file.id})`)
+            .join('\n')}`,
+        })
+      } else {
+        res.json({ message: 'No files found.' })
+      }
+    })
+  } catch (e) {
+    res.status(500).send('Fail to download pdf!')
+  }
+})
 
 const loadCVPrompt = () => {
   const drive = google.drive({
-    version: "v3",
+    version: 'v3',
     auth: OAuth2ClientInstance.instance,
-  });
+  })
 
   return new Promise((resolve, reject) => {
     drive.files.list((err, response) => {
-      const files = response.data.files;
+      const files = response.data.files
       if (files.length) {
         const pdfFileId = files.filter(
-          (file) => file.mimeType == "application/pdf"
-        )[0].id;
+          (file) => file.mimeType == 'application/pdf'
+        )[0].id
         //   const dest = fs.createWriteStream("test.pdf");
 
         drive.files.get(
-          { fileId: pdfFileId, alt: "media" },
-          { responseType: "arraybuffer" },
+          { fileId: pdfFileId, alt: 'media' },
+          { responseType: 'arraybuffer' },
           async (err, { data }) => {
             if (err) {
-              console.log(err);
-              resolve("");
+              console.log(err)
+              resolve('')
             }
-            const buffer = Buffer.from(data);
-            const cvPrompt = await convertPdfToText(buffer);
-            resolve(cvPrompt);
+            const buffer = Buffer.from(data)
+            const cvPrompt = await convertPdfToText(buffer)
+            resolve(cvPrompt)
           }
-        );
+        )
       } else {
       }
-    });
-  });
-};
+    })
+  })
+}
 
-router.get("/check-cv-prompt", async (req, res) => {
+router.get('/check-cv-prompt', async (req, res) => {
   const drive = google.drive({
-    version: "v3",
+    version: 'v3',
     auth: OAuth2ClientInstance.instance,
-  });
+  })
 
   drive.files.list((err, response) => {
     if (err) {
-      console.error("Error listing files:", err);
-      res.send("Error listing files");
+      console.error('Error listing files:', err)
+      res.send('Error listing files')
     }
-    const files = response.data.files;
+    const files = response.data.files
     if (files.length) {
       const pdfFileId = files.filter(
-        (file) => file.mimeType == "application/pdf"
-      )[0].id;
+        (file) => file.mimeType == 'application/pdf'
+      )[0].id
       //   const dest = fs.createWriteStream("test.pdf");
 
       drive.files.get(
-        { fileId: pdfFileId, alt: "media" },
-        { responseType: "arraybuffer" },
+        { fileId: pdfFileId, alt: 'media' },
+        { responseType: 'arraybuffer' },
         async (err, { data }) => {
           if (err) {
-            console.log(err);
-            return;
+            console.log(err)
+            return
           }
-          console.log("data", data);
-          const buffer = Buffer.from(data);
-          const cvPrompt = await convertPdfToText(buffer);
-          console.log("cvPrompt", cvPrompt);
+          console.log('data', data)
+          const buffer = Buffer.from(data)
+          const cvPrompt = await convertPdfToText(buffer)
+          console.log('cvPrompt', cvPrompt)
           //   data
           //     .on("end", () => {})
           //     .on("error", (err) => {
@@ -231,31 +225,68 @@ router.get("/check-cv-prompt", async (req, res) => {
           //       console.log("dest", dest);
           //     });
         }
-      );
+      )
 
       res.send(
         `Files found:\n${files
           .map((file) => `${file.name} (${file.id})`)
-          .join("\n")}`
-      );
+          .join('\n')}`
+      )
     } else {
-      res.send("No files found.");
+      res.send('No files found.')
     }
-  });
-});
+  })
+})
 
-router.get(
-  '/generate-interview-questions',
+router.post(
+  '/find-jobs-for-cv',
+  // verifyToken,
+  upload.single('file'),
   async (req, res) => {
-    const jdId = req.query.jd;
-    const jd = await JobDescription.findById(jdId);
-    if (jd) {
-      const prompt = `As a recruiter, my objective is to design a comprehensive set of interview questions that effectively illuminate the interviewees' proficiency with this job description:
-      ${jd.data}.
-      Can you assist me in creating an interview protocol with 10 multiple choices questions about the Responsibilities in job description and the expectation answer?`;
-      const stringData = await chatGPTAzure(prompt);
+    try {
+      const data = fs.readFileSync(req.file.path)
+      const cvText = await convertPdfToText(data)
+      console.log(cvText)
+      // const oauth2Client = getOAuth2Client()
+      // oauth2Client.setCredentials(req.body.token)
+      // const drive = google.drive({
+      //   version: 'v3',
+      //   auth: oauth2Client,
+      // })
+      const jobDescriptions = [
+        { data: 'Senior java developer', jobTitle: 'Senior java developer' },
+        { data: 'React native developer', jobTitle: 'React native developer' },
+      ]
+      // const jobDescriptions = await JobDescription.find().exec()
+      const results = await Promise.all(
+        jobDescriptions.map(async (jd) => {
+          const prompt = `
+              I have a job with description: ${jd.data} and title: ${jd.jobTitle}.
+              The candidate CV: ${cvText}.
+              Please, help me to calculate matching between the CV and JD, the result in percent, just give me only the result's number`
 
-      const prompt1 = `Please help me to convert this string: ${stringData} to the JSON string with structure:
+          const result = await chatGPTAzure(prompt)
+          return Number(result.replace('%', '')) || 0
+        })
+      )
+
+      //remove file after used
+      fs.unlink(req.file.path)
+      res.send(`OK ${results.sort((a, b) => b - a).filter((rs) => rs !== 0)}`)
+    } catch (e) {}
+  }
+)
+
+router.get('/generate-interview-questions', async (req, res) => {
+  const jdId = req.query.jd
+  const jd = await JobDescription.findById(jdId)
+  if (jd) {
+    const prompt = `As a recruiter, my objective is to design a comprehensive set of interview questions that effectively illuminate the interviewees' proficiency with this job description:
+      ${jd.data}.
+      Can you assist me in creating an interview protocol with 10 multiple choices questions about the Responsibilities in job description and the expectation answer?`
+    const stringData = await chatGPTAzure(prompt)
+
+    const prompt1 = `Please help me to convert this string: ${stringData} to the JSON string with structure:
       {
         "data": [
           {
@@ -265,14 +296,13 @@ router.get(
           }
         ]
       }
-      `;
-      const jsonData = await chatGPTAzure(prompt1);
+      `
+    const jsonData = await chatGPTAzure(prompt1)
 
-      res.json(jsonData);
-    } else {
-      res.json({});
-    }
+    res.json(jsonData)
+  } else {
+    res.json({})
   }
-)
+})
 
-module.exports = router;
+module.exports = router
