499bc7efbb51475f031b320cc00952ce3087d9f2
kietnguyendev99 <kiet.nguyen2@mservice.com.vn>
2023-09-21 15:38:37 +0700

    update: try-catch and generate interview

--- src/api/mainFeatures.js
+++ src/api/mainFeatures.js
@@ -7,7 +7,6 @@ const { getOAuth2Client } = require('../utils/oauth2Client')
 const User = require('../models/UserModel')
 const JobDescription = require('../models/JobDescriptionModel')
 const multer = require('multer')
-const { file } = require('googleapis/build/src/apis/file')
 const { convertPdfToText, getNumberResult } = require('../utils')
 const upload = multer({ dest: 'uploads/' })
 
@@ -55,50 +54,57 @@ const chatGPTAzure = (prompt) => {
   })
 }
 
-router.post('/generate/job-description', verifyToken, async (req, res) => {
-  try {
-    const { jobTitle = '', skills = [], extras = '' } = req.body;
+router.post(
+  '/generate/job-description',
+  verifyToken,
+  async (req, res) => {
+    try {
+      const { jobTitle = '', skills = [], extras = '' } = req.body;
 
-    const prompt = `
+      const prompt = `
       Please generate a job description for a ${jobTitle}. 
       The ideal candidate should have experience in ${skills.join()}. 
       ${extras}.
       Please include the job responsibilities and required qualifications.`;
 
-    const result = await chatGPTAzure(prompt);
-    const oauth2Client = getOAuth2Client();
-    oauth2Client.setCredentials(req.body.token);
-    const drive = google.drive({ version: 'v3', auth: oauth2Client });
-    const googleUser = (await drive.about.get({ fields: 'user' })).data.user;
-    const user = await User.findOne({ id: googleUser.permissionId });
-    await JobDescription.create({ jobTitle, data: result, createdBy: user, status: true, isDeleted: false });
+      const result = await chatGPTAzure(prompt);
+      const oauth2Client = getOAuth2Client();
+      oauth2Client.setCredentials(req.body.token);
+      const drive = google.drive({ version: 'v3', auth: oauth2Client });
+      const googleUser = (await drive.about.get({ fields: 'user' })).data.user;
+      const user = await User.findOne({ id: googleUser.permissionId });
+      await JobDescription.create({ jobTitle, data: result, createdBy: user, status: true, isDeleted: false });
 
-    res.json({ data: result })
-  } catch (e) {
-    res.status(500).send('Generate JD was failed!' + e)
-  }
-})
+      res.json({ data: result })
+    } catch (e) {
+      res.status(500).send('Generate JD was failed!' + e);
+    }
+  })
 
 router.post(
   '/list/drive-folders',
   verifyToken,
   async (req, res) => {
-    const oauth2Client = getOAuth2Client();
-    oauth2Client.setCredentials(req.body.token);
-    const drive = google.drive({
-      version: 'v3',
-      auth: oauth2Client,
-    });
+    try {
+      const oauth2Client = getOAuth2Client();
+      oauth2Client.setCredentials(req.body.token);
+      const drive = google.drive({
+        version: 'v3',
+        auth: oauth2Client,
+      });
 
-    const listData = await drive.files.list({
-      q: `'root' in parents and mimeType='application/vnd.google-apps.folder'`,
-    });
-    const folders = listData.data.files?.map(file => ({
-      id: file.id,
-      folderName: file.name
-    })) || [];
+      const listData = await drive.files.list({
+        q: `'root' in parents and mimeType='application/vnd.google-apps.folder'`,
+      });
+      const folders = listData.data.files?.map(file => ({
+        id: file.id,
+        folderName: file.name
+      })) || [];
 
-    res.json({ data: folders });
+      res.json({ data: folders });
+    } catch (e) {
+      res.status(500).send('Get list folders was failed!' + e);
+    }
   }
 );
 
@@ -106,101 +112,56 @@ router.post(
   '/find-cvs-for-job',
   verifyToken,
   async (req, res) => {
-    const oauth2Client = getOAuth2Client();
-    oauth2Client.setCredentials(req.body.token);
-    const drive = google.drive({
-      version: 'v3',
-      auth: oauth2Client,
-    });
-    let { folderId, jdId, top } = req.body;
-    top = Number(top) || 3;
-    const jd = await JobDescription.findById(jdId);
-    if (jd) {
-      const response = await drive.files.list({
-        q: `'${folderId}' in parents`,
+    try {
+      const oauth2Client = getOAuth2Client();
+      oauth2Client.setCredentials(req.body.token);
+      const drive = google.drive({
+        version: 'v3',
+        auth: oauth2Client,
       });
-      const files = response.data.files;
-      if (files.length) {
-        const pdfFiles = files.filter(
-          (file) => file.mimeType == 'application/pdf'
-        );
+      let { folderId, jdId, top } = req.body;
+      top = Number(top) || 3;
+      const jd = await JobDescription.findById(jdId);
+      if (jd) {
+        const response = await drive.files.list({
+          q: `'${folderId}' in parents`,
+        });
+        const files = response.data.files;
+        if (files.length) {
+          const pdfFiles = files.filter(
+            (file) => file.mimeType == 'application/pdf'
+          );
 
-        const results = await Promise.all(pdfFiles.map(async file => {
-          const dataBuffer = await drive.files.get({ fileId: file.id, alt: 'media' }, { responseType: 'arraybuffer' });
-          const text = await convertPdfToText(dataBuffer);
-          const prompt = `
-          I have a job with description: ${jd.data}.
-          The candidate CV: ${text}.
-          Please, help me to calculate matching between the CV and JD, the result in percent, just give me only the result's number`
+          const results = await Promise.all(pdfFiles.map(async file => {
+            const dataBuffer = await drive.files.get({ fileId: file.id, alt: 'media' }, { responseType: 'arraybuffer' });
+            const text = await convertPdfToText(dataBuffer);
+            const prompt = `
+            I have a job with description:
+            ${jd.data.trim()}
 
-          const result = await chatGPTAzure(prompt);
-          return {
-            url: `https://drive.google.com/file/d/${file.id}/view`,
-            percent: getNumberResult(result)
-          };
-        }));
+            And the candidate CV:
+            ${text.trim()}
 
-        res.json({ data: results.sort((a, b) => b.percent - a.percent).filter((rs) => rs.percent !== 0).slice(0, top) });
+            Please, help me to calculate matching between the CV and JD, the result in percent, just give me only the result's number`;
+
+            const result = await chatGPTAzure(prompt);
+            return {
+              url: `https://drive.google.com/file/d/${file.id}/view`,
+              percent: getNumberResult(result)
+            };
+          }));
+
+          res.json({ data: results.sort((a, b) => b.percent - a.percent).filter((rs) => rs.percent !== 0).slice(0, top) });
+        } else {
+          res.json({ data: [] });
+        }
       } else {
         res.json({ data: [] });
       }
-    } else {
-      res.json({ data: [] });
+    } catch (e) {
+      res.status(500).send('Find CVs for Job was failed!' + e);
     }
   })
-
-router.get('/check-cv-prompt', async (req, res) => {
-  const drive = google.drive({
-    version: 'v3',
-    auth: OAuth2ClientInstance.instance,
-  })
-
-  drive.files.list((err, response) => {
-    if (err) {
-      console.error('Error listing files:', err)
-      res.send('Error listing files')
-    }
-    const files = response.data.files
-    if (files.length) {
-      const pdfFileId = files.filter(
-        (file) => file.mimeType == 'application/pdf'
-      )[0].id
-      //   const dest = fs.createWriteStream("test.pdf");
-
-      drive.files.get(
-        { fileId: pdfFileId, alt: 'media' },
-        { responseType: 'arraybuffer' },
-        async (err, { data }) => {
-          if (err) {
-            console.log(err)
-            return
-          }
-          console.log('data', data)
-          const buffer = Buffer.from(data)
-          const cvPrompt = await convertPdfToText(buffer)
-          console.log('cvPrompt', cvPrompt)
-          //   data
-          //     .on("end", () => {})
-          //     .on("error", (err) => {
-          //       console.log(err);
-          //       return process.exit();
-          //     })
-          //     .pipe((dest) => {
-          //       console.log("dest", dest);
-          //     });
-        }
-      )
-
-      res.send(
-        `Files found:\n${files
-          .map((file) => `${file.name} (${file.id})`)
-          .join('\n')}`
-      )
-    } else {
-      res.send('No files found.')
-    }
-  })
-})
 
 router.post(
   '/find-jobs-for-cv',
@@ -238,70 +199,80 @@ router.post(
       fs.unlinkSync(req.file.path);
       res.json(results.sort((a, b) => b.percent - a.percent).filter((rs) => rs.percent !== 0).slice(0, top));
     } catch (e) {
-      console.log(e);
+      res.status(500).send('Find Jobs for CV was failed!' + e);
     }
   }
 )
 
-router.get('/generate-interview-questions', async (req, res) => {
-  const jdId = req.query.jd
-  const jd = await JobDescription.findById(jdId)
-  if (jd) {
-    const prompt = `As a recruiter, my objective is to design a comprehensive set of interview questions that effectively illuminate the interviewees' proficiency with this job description:
-      ${jd.data}.
-      Can you assist me in creating an interview protocol with 10 multiple choices questions about the Responsibilities in job description and the expectation answer?`
-    const stringData = await chatGPTAzure(prompt)
+router.get(
+  '/generate-interview-questions',
+  async (req, res) => {
+    try {
+      const jdId = req.query.jd
+      const jd = await JobDescription.findById(jdId)
+      if (jd) {
+        const prompt = `As a recruiter, my objective is to design a comprehensive set of interview questions that effectively illuminate the interviewees' proficiency with this job description:
+        ${jd.data}.
+        Can you assist me in creating an interview protocol with 10 multiple choices questions about the Responsibilities in job description and the expectation answer?`
+        const stringData = await chatGPTAzure(prompt)
 
-    const prompt1 = `Please help me to convert this string: ${stringData} to the JSON string with structure:
-      {
-        "data": [
-          {
-            "question": // a string question,
-            "options": // an array string,
-            "correct_answer": // the index of correct answer
+        const prompt1 = `Please help me to convert this string: ${stringData} to the JSON string with structure:
+        {
+          "data": [
+            {
+              "question": // a string question,
+              "options": // an array string,
+              "correct_answers": // an array index of correct answer
+            }
+          ]
+        }`;
+        const data = await chatGPTAzure(prompt1) || "";
+        const firstIndexOfBracket = data.indexOf("{");
+        let lastIndexOfBracket = -1;
+        for (let i = (data?.length || 0) - 1; i >= 0; i--) {
+          if (data?.[i] === "}") {
+            lastIndexOfBracket = i;
+            break;
           }
-        ]
-      }`;
-    const data = await chatGPTAzure(prompt1) || "";
-    const firstIndexOfBracket = data.indexOf("{");
-    let lastIndexOfBracket = -1;
-    for (let i = (data?.length || 0) - 1; i >= 0; i--) {
-      if (data?.[i] === "}") {
-        lastIndexOfBracket = i;
-        break;
-      }
-    }
-    const jsonData = data.substring(firstIndexOfBracket, lastIndexOfBracket + 1)
+        }
+        const jsonData = data.substring(firstIndexOfBracket, lastIndexOfBracket + 1);
 
-    res.json(jsonData)
-  } else {
-    res.json({})
-  }
-})
+        res.json(JSON.parse(jsonData))
+      } else {
+        res.json({})
+      }
+    } catch (e) {
+      res.status(500).send('Generate interview questions was failed!' + e);
+    }
+  })
 
 router.post(
   '/list/job-descriptions',
   verifyToken,
   async (req, res) => {
-    const { id = null, status = null, token } = req.body;
-    console.log(status)
+    try {
+      const { id = null, status = null, token } = req.body;
+      console.log(status)
 
-    const oauth2Client = getOAuth2Client();
-    oauth2Client.setCredentials(token);
-    const drive = google.drive({ version: 'v3', auth: oauth2Client });
-    const googleUser = (await drive.about.get({ fields: 'user' })).data.user;
-    const user = await User.findOne({ id: googleUser.permissionId });
+      const oauth2Client = getOAuth2Client();
+      oauth2Client.setCredentials(token);
+      const drive = google.drive({ version: 'v3', auth: oauth2Client });
+      const googleUser = (await drive.about.get({ fields: 'user' })).data.user;
+      const user = await User.findOne({ id: googleUser.permissionId });
 
-    const jds = await JobDescription.find({
-      createdBy: user,
-      isDeleted: false,
-      ...((typeof status == "boolean") ? { status } : {}),
-      ...(id ? { id } : {})
-    });
+      const jds = await JobDescription.find({
+        createdBy: user,
+        isDeleted: false,
+        ...((typeof status == "boolean") ? { status } : {}),
+        ...(id ? { id } : {})
+      });
 
-    res.json({
-      data: jds || []
-    });
+      res.json({
+        data: jds || []
+      });
+    } catch (e) {
+      res.status(500).send('Get list job descriptions was failed!' + e);
+    }
   }
 );
 
@@ -309,35 +280,39 @@ router.post(
   '/update/job-description',
   verifyToken,
   async (req, res) => {
-    const {
-      id,
-      status = null,
-      isDeleted = null,
-      data = null,
-      jobTitle = null
-    } = req.body;
+    try {
+      const {
+        id,
+        status = null,
+        isDeleted = null,
+        data = null,
+        jobTitle = null
+      } = req.body;
 
-    const jd = await JobDescription.findById(id);
-    if (!jd) {
-      res.json({ message: 'Not existed!' });
-    } else {
-      if (typeof status == 'boolean') {
-        jd.status = status;
+      const jd = await JobDescription.findById(id);
+      if (!jd) {
+        res.json({ message: 'Not existed!' });
+      } else {
+        if (typeof status == 'boolean') {
+          jd.status = status;
+        }
+        if (typeof isDeleted == 'boolean') {
+          jd.isDeleted = isDeleted;
+        }
+        if (!!data) {
+          jd.data = data;
+        }
+        if (!!jobTitle) {
+          jd.jobTitle = jobTitle;
+        }
+        await jd.save();
+        res.json({ message: 'Success' });
       }
-      if (typeof isDeleted == 'boolean') {
-        jd.isDeleted = isDeleted;
-      }
-      if (!!data) {
-        jd.data = data;
-      }
-      if (!!jobTitle) {
-        jd.jobTitle = jobTitle;
-      }
-      await jd.save();
-      res.json({ message: 'Success' });
+      await jd.save()
+      res.json({ message: 'Success' })
+    } catch (e) {
+      res.status(500).send('Update job description was failed!' + e);
     }
-    await jd.save()
-    res.json({ message: 'Success' })
   }
 )
 
